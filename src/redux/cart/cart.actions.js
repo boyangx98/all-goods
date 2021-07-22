58db49d797552e3e1c4c8b31ad1549944fabf367
boyangx <boyangx@umich.edu>
2021-07-22 12:49:28 -0400

    styled add to cart button, added addItem action to cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -2,6 +2,13 @@ import { CartActionTypes } from "./cart.types";
 
 export const toggleCartHidden = () => {
     return {
-        type: CartActionTypes.TOGGLE_CART_HIDDEN
-    }
-}
+        type: CartActionTypes.TOGGLE_CART_HIDDEN,
+    };
+};
+
+export const addItem = (item) => {
+    return {
+        type: CartActionTypes.ADD_ITEM,
+        payload: item,
+    };
+};
